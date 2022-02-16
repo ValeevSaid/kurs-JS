59f9c1249fc2d360d57996e97736c4e383fbea8e
Said Valeev <venomzi@mail.ru>
2022-02-16 22:28:10 +0300

    fix

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 "use strict";
 
+function getRandomInt() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+let random = getRandomInt();
+console.log(random);
 const isNumber = function (num) {
   return !isNaN(parseFloat(num)) && isFinite(num);
 };
 
 function bot(x) {
-  let userAnswer = +prompt("Угадай число от 1 до 100");
-  if (userAnswer != null) {
-    userAnswer = +prompt("Введите число");
-    if (!isNumber(userAnswer)) {
-      +prompt("Введите число");
-    } else if (userAnswer < x) {
-      +prompt("Загаданное число больше");
-    } else if (userAnswer > x) {
-      +prompt("Загаданное число меньше!");
-    } else if (userAnswer === x) {
-      alert("Поздравляю, Вы угадали!!!");
-    }
-  } else {
-    alert("Игра окончена");
-  }
+  let userAnswer = prompt("Угадай число от 1 до 100");
 
+  if (+userAnswer == null) {
+    alert("Игра окончена");
+  } else if (!isNumber(+userAnswer)) {
+    userAnswer += +prompt("Введите число");
+  } else if (+userAnswer < x) {
+    userAnswer += +prompt("Загаданное число больше");
+  } else if (+userAnswer > x) {
+    userAnswer += +prompt("Загаданное число меньше!");
+  } else if (+userAnswer === x) {
+    alert("Поздравляю, Вы угадали!!!");
+  }
   bot(x);
 }
 
-bot(40);
+bot(random);
