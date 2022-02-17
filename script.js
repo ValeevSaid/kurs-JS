c99205b4cb48568c4dbc571dbcdfa68388362477
Said Valeev <venomzi@mail.ru>
2022-02-18 00:44:23 +0300

    fix bot()

--- script.js
+++ script.js
@@ -4,43 +4,40 @@ let random;
 let userAnswer;
 function getRandomInt() {
   random = Math.floor(Math.random() * 100) + 1;
-  return random;
 }
-random = getRandomInt();
 
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+function bot() {
+  let userAnswer = prompt("Угадай число от 1 до 100");
+
+  if (userAnswer == null) {
+    alert("До свидания!");
+    return;
+  }
+
+  if (!isNumber(userAnswer)) {
+    alert("Необходимо ввести число, а не строку!");
+    bot();
+    return;
+  }
+  if (userAnswer > random) {
+    alert("Необходимо ввести число поменьше!");
+    bot();
+    return;
+  }
+  if (userAnswer < random) {
+    alert("Необходимо ввести поменьше!");
+    bot();
+    return;
+  }
+  if (userAnswer == random) {
+    alert("Поздравляю ты выиграл!");
+    return;
+  }
+}
+
+bot();
 console.log(random);
-
-function isNumber(num) {
-  if (!isNaN(parseFloat(num)) && isFinite(num)) {
-    bot(random);
-  } else {
-    question();
-  }
-}
-
-function question() {
-  userAnswer = +prompt("Угадай число от 1 до 100");
-  if (userAnswer === null) {
-    alert("Игра окончена");
-    return;
-  } else {
-    return userAnswer;
-  }
-}
-
-function bot(x) {
-  if (userAnswer < x) {
-    userAnswer = +prompt("Загаданное число больше");
-    bot();
-  } else if (userAnswer > x) {
-    userAnswer = +prompt("Загаданное число меньше!");
-    bot();
-  }
-  if (userAnswer === x) {
-    alert("Поздравляю, Вы угадали!!!");
-    return;
-  }
-}
-question();
-isNumber(userAnswer);
-console.log(userAnswer, typeof userAnswer);
